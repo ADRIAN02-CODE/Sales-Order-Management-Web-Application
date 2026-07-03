cfaf1f40d310128306c74cf40bb32ec674280ce4
ADRIAN02-CODE <adrianvethanayagam@gmail.com>
2026-07-03 15:32:18 +0530

    build: initialize build artifacts and SQLite database files for the application

--- Infrastructure/obj/Debug/net8.0/SalesOrderManagement.Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/SalesOrderManagement.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesOrderManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4697daebde0b981ad9883fdc98e971542b66d1e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesOrderManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesOrderManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
